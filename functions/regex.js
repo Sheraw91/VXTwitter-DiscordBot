b82bbc39e4b10835b318fa705063b667e3938624
Sheraw91 <theogrelet05@gmail.com>
2023-06-09 10:49:48 +0200

    :ambulance: fix: remove requests to Twitter API

--- functions/regex.js
+++ functions/regex.js
@@ -1,42 +1,47 @@
-const { generateFixedUrl } = require('./urlUtils')
-const { analyzeTweet, isVideo, isQuotingVideo } = require('./twitterUtils')
-const regex = /https?:\/\/twitter\.com(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)/img
+const { generateFixedUrl } = require("./urlUtils");
+// const { analyzeTweet, isVideo, isQuotingVideo } = require("./twitterUtils");
+const regex =
+  /https?:\/\/twitter\.com(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)/gim;
 
 const hasTwitterLink = (message) => {
-  const regexLink = new RegExp(regex)
-  return regexLink.test(message)
-}
+  const regexLink = new RegExp(regex);
+  return regexLink.test(message);
+};
 
 const getVxTwitterLink = async (message) => {
-  const regexLink = new RegExp(regex)
+  const regexLink = new RegExp(regex);
   const resp = {
     message: message,
-    video: 0
-  }
+    video: 0,
+  };
 
-  const links = message.match(regexLink)
+  const links = message.match(regexLink);
 
   for (const link of links) {
-    const tweetId = link.split('/').at(-1).split('?')[0]
-    const analyzedTweet = await analyzeTweet(tweetId)
+    resp.message = resp.message.replace(link, generateFixedUrl(link));
+    resp.video += 1;
 
-    const containsVideo = await isVideo(analyzedTweet)
-    if (containsVideo) {
-      resp.message = resp.message.replace(link, generateFixedUrl(link))
-      resp.video += 1
-    } else {
-      // Check that the tweet is not quoting a tweet with video
-      if (await isQuotingVideo(analyzedTweet)) {
-        resp.message = resp.message.replace(link, generateFixedUrl(link))
-        resp.video += 1
-      }
-    }
+    /* API IS NOT FREE ANYMORE */
+    // const tweetId = link.split("/").at(-1).split("?")[0];
+    // const analyzedTweet = await analyzeTweet(tweetId);
+    // resp.message = resp.message.replace(link, generateFixedUrl(link));
+    // const containsVideo = await isVideo(analyzedTweet)
+    // if (containsVideo) {
+    //   resp.message = resp.message.replace(link, generateFixedUrl(link))
+    //   resp.video += 1
+    // } else {
+    //   // Check that the tweet is not quoting a tweet with video
+    //   if (await isQuotingVideo(analyzedTweet)) {
+    //     resp.message = resp.message.replace(link, generateFixedUrl(link))
+    //     resp.video += 1
+    //   }
+    // }
   }
 
-  return resp
-}
+  return resp;
+};
 
 module.exports = {
   hasTwitterLink,
-  getVxTwitterLink
-}
+  getVxTwitterLink,
+};
