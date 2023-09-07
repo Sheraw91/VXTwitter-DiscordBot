943b9353988c94d4b8944892f0b86aa123701d91
Damien <48047820+JustDams@users.noreply.github.com>
2023-09-07 11:28:35 +0200

    update function generateFixedUrl

--- functions/urlUtils.js
+++ functions/urlUtils.js
@@ -2,16 +2,12 @@ const { baseUrl } = require('../config.json')
 
 const getTweetIdFromUrl = (url) => url.split('/').at(-1).split('?')[0]
 
-const generateFixedUrl = (url) => {
-  const twitterUrl = /twitter.com/gmi
-  const xUrl = /x.com/gmi
+const generateFixedUrl = (url, hostname) => {
   let fixedUrl = url.split('/')
 
   fixedUrl.splice(0, 2)
   fixedUrl = fixedUrl.join('/')
-
-  if (twitterUrl.test(fixedUrl)) fixedUrl = fixedUrl.replace('twitter.com', baseUrl)
-  else if (xUrl.test(fixedUrl)) fixedUrl = fixedUrl.replace('x.com', baseUrl)
+  fixedUrl = fixedUrl.replace(hostname, baseUrl)
 
   return fixedUrl
 }
