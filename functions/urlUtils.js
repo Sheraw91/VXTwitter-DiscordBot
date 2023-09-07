8bbaed9e6b6eefab026b4694b62942d8d869ca9a
Damien <48047820+JustDams@users.noreply.github.com>
2023-09-07 12:08:31 +0200

    refacto

--- functions/urlUtils.js
+++ functions/urlUtils.js
@@ -2,13 +2,8 @@ const { baseUrl } = require('../config.json')
 
 const getTweetIdFromUrl = (url) => url.split('/').at(-1).split('?')[0]
 
-const generateFixedUrl = (url, hostname) => {
-  let fixedUrl = url.split('/')
-
-  fixedUrl.splice(0, 2)
-  fixedUrl = fixedUrl.join('/')
-  fixedUrl = fixedUrl.replace(hostname, baseUrl)
-
+const generateFixedUrl = (url) => {
+  let fixedUrl = `${baseUrl}${url}`
   return fixedUrl
 }
 
