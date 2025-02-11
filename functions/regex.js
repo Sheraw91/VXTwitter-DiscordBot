d5d259d75aa8ad8a2f7d2e91f12a07f792ad5d35
Sheraw91 <theogrelet05@gmail.com>
2025-02-11 14:57:22 +0100

    feat: add validation of media nor quotted tweet

--- functions/regex.js
+++ functions/regex.js
@@ -1,7 +1,8 @@
 const { generateFixedUrl } = require("./urlUtils");
 // const { analyzeTweet, isVideo, isQuotingVideo } = require("./twitterUtils");
-const regex = /https?:\/\/(twitter|x)\.com(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)/gim;
-
+const axios = require("axios");
+const regex =
+  /https?:\/\/(twitter|x)\.com(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)/gim;
 
 const hasTwitterLink = (message) => {
   const regexLink = new RegExp(regex);
@@ -19,32 +20,46 @@ const getVxTwitterLink = async (message) => {
 
   for (const link of links) {
     const result = new RegExp(regex).exec(link);
-    // Getting url after hostname
     const url = result[2];
-    
-    resp.message = resp.message.replace(link, generateFixedUrl(url));
-    resp.video += 1;
 
-    /* API IS NOT FREE ANYMORE */
+    /* NO API */
+    // Getting url after hostname
+    // resp.message = resp.message.replace(link, generateFixedUrl(url));
+    // resp.video += 1;
+
+    /* TWITTER API - NOT FREE ANYMORE */
     // const tweetId = link.split("/").at(-1).split("?")[0];
     // const analyzedTweet = await analyzeTweet(tweetId);
     // resp.message = resp.message.replace(link, generateFixedUrl(link));
-    // const containsVideo = await isVideo(analyzedTweet)
+    // const containsVideo = await isVideo(analyzedTweet);
     // if (containsVideo) {
-    //   resp.message = resp.message.replace(link, generateFixedUrl(link))
-    //   resp.video += 1
+    //   resp.message = resp.message.replace(link, generateFixedUrl(link));
+    //   resp.video += 1;
     // } else {
     //   // Check that the tweet is not quoting a tweet with video
     //   if (await isQuotingVideo(analyzedTweet)) {
-    //     resp.message = resp.message.replace(link, generateFixedUrl(link))
-    //     resp.video += 1
+    //     resp.message = resp.message.replace(link, generateFixedUrl(link));
+    //     resp.video += 1;
     //   }
     // }
+
+    /* VXTWITTER API - FREE */
+    const tweetId = link.split("/").at(-1).split("?")[0];
+    const vxtwitter = await axios
+      .get(`https://api.vxtwitter.com/Twitter/status/${tweetId}`)
+      .then((resp) => resp.data)
+      .catch(console.error);
+    if (vxtwitter.hasMedia) {
+      resp.message = resp.message.replace(link, generateFixedUrl(url));
+      resp.video += 1;
+    } else if (vxtwitter.qrt) {
+      resp.message = resp.message.replace(link, generateFixedUrl(url));
+      resp.video += 1;
+    }
   }
 
   return resp;
 };
-
 
 module.exports = {
   hasTwitterLink,
